bc3342ad9eee3ff50636d0a8ebbb2fa8c79bdb07
Moiseis Cruz <moizzito@gmail.com>
2023-10-13 18:45:03 -0300

    Ligue a api ao useEffect

--- src/components/advice-generator/index.js
+++ src/components/advice-generator/index.js
@@ -5,22 +5,32 @@ async function createAdvice() {
     return await response.json()
 }
 
-console.log(await createAdvice());
-
 export const Main = () => {
 
-    const [ deck, setDeck ] = useState({slip})
+    const [ deck, setDeck ] = useState({slip: []})
 
     useEffect(() => {
         async function dataFetch() {
             const adviceId = await createAdvice()
 
-            setDeck()
+            setDeck({slip: [adviceId]})
         }
-    })
+        dataFetch()
+    }, [])
 
     return(
-        <h1>Título</h1>
+        <section>
+            {
+                deck.slip.map((item, index) => {
+                    return(
+                        <div key={index}>
+                            <h2>{item.slip.id}</h2>
+                            <p>{item.slip.advice}</p>
+                        </div>
+                    )
+                })
+            }
+        </section>
     )
 }
 
