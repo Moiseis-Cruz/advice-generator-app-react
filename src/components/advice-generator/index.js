264fd25c58012e2299648fa92d1a8217b807fcd3
Moiseis Cruz <moizzito@gmail.com>
2023-10-13 21:21:21 -0300

    Estilizei Section, comecei o Card.

--- src/components/advice-generator/index.js
+++ src/components/advice-generator/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import ImgDiceButton from "../imgs/icon-dice.svg"
 import ImgGreat from "../imgs/pattern-divider-desktop.svg"
 import ImgSmall from "../imgs/pattern-divider-mobile.svg"
+import styled from "styled-components";
 
 async function createAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice")
@@ -22,11 +23,11 @@ export const Main = () => {
     }, [])
 
     return(
-        <section>
+        <Section>
             {
                 deck.slip.map((item, index) => {
                     return(
-                        <div key={index}>
+                        <Card key={index}>
                             <h2>ADVICE #{item.slip.id}</h2>
                             <p>{item.slip.advice}</p>
 
@@ -38,10 +39,31 @@ export const Main = () => {
                             <button>
                                 <img src={ImgDiceButton} />
                             </button>
-                        </div>
+                        </Card>
                     )
                 })
             }
-        </section>
+        </Section>
     )
 }
+
+const Section = styled.section`
+    max-width: 1440px;
+    height: 100vh;
+    margin: 0 auto;
+    background-Color: #1F2632;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Card = styled.div`
+    max-width: 540px;
+    width: 100%;
+    height: 332px;
+    border-radius: 15px;
+    background-color: #323A49;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
